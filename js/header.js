e0494b56ce19b39f309d3019b7b92bbbf7a70512
thespiritbomb <rana.niyati4@gmail.com>
2019-08-09 03:05:53 +0530

    Indentation and Adding Comments - 1

--- js/header.js
+++ js/header.js
@@ -5,18 +5,23 @@ var signInBtn=document.getElementById("headerSignIn");
 var closeSignUpModal=document.getElementsByClassName("closeModal")[0];
 var closeSignInModal=document.getElementsByClassName("closeModal")[1];
 var link=document.getElementById("signInModalLink");
+// Open Sign Up Modal on clicking Sign Up Button
 signUpBtn.onclick=function(){
     signUpModal.style.display="flex";
 }
+// Hide Sign Up Modal on clicking cross Icon
 closeSignUpModal.onclick=function(){
     signUpModal.style.display="none";
 }
+// Open Sign In Modal on clicking Sign In Button
 signInBtn.onclick=function(){
     signInModal.style.display="flex";
 }
+// Close Sign In Modal on clicking cross Icon
 closeSignInModal.onclick=function(){
     signInModal.style.display="none";
 }
+// Open Sign Up Modal on clicking Sign Up Link in the Sign in Modal
 link.onclick=function(){
     signInModal.style.display="none";
     signUpModal.style.display="flex";
--- js/index.js
+++ js/index.js
@@ -2,15 +2,19 @@ var allPostsBtn=document.getElementById("allPostsBtn");
 var createPostBtn=document.getElementById("createPostBtn");
 var createPostModal=document.getElementById("createPostModalDiv")
 var closeCreatePostModal=document.getElementsByClassName("closeModal")[2];
+// Display Create Post modal on clicking button
 createPostBtn.onclick=function(){
     createPostModal.style.display="flex";
 }
+// Hide Create Post modal on clicking cross icon
 closeCreatePostModal.onclick=function(){
     createPostModal.style.display="none";
 }
+// Opening new page on clicking All Posts Button
 allPostsBtn.onclick=function(){
     window.open("file:./html/bloglist.html","_self");
 }
+// Opening home page on clicking the ScriBBler Logo
 document.getElementById("headerLogoDiv").onclick=function(){
     window.open("file:./index.html","_self");
 }
