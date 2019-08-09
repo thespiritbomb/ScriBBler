b87d628777e81a8d30aac703356fb45ff6b118ac
thespiritbomb <rana.niyati4@gmail.com>
2019-08-09 18:36:46 +0530

    Indentation and Comments Added

--- js/bloglist.js
+++ js/bloglist.js
@@ -1,20 +1,24 @@
 var deleteModal=document.getElementById("deletePostModalDiv");
 var trashIcon=document.getElementsByClassName("post-trash");
 var noBtn=document.getElementById("noDeletePostBtn");
+// Open the Delete Modal on clicking any of the Trash Icons
 for(var i=0;i<trashIcon.length;i++){
     trashIcon[i].onclick=function(){
         deleteModal.style.display="flex";
     }
 }
+// Close the Delete Modal on clicking the NO Button
 noBtn.onclick=function(){
         deleteModal.style.display="none";
     }
 var moreIcon=document.getElementsByClassName("post-more");
+// Open the Post.html page on clicking any of the More Icons
 for(var i=0;i<moreIcon.length;i++){
     moreIcon[i].onclick=function(){
         window.open("file:./post.html","_self");
     }
 }
+// Open the Index.html page on clicking the ScriBBler Logo
 document.getElementById("headerLogoDiv").onclick=function(){
     window.open("file:../index.html","_self");
 }
--- js/post.js
+++ js/post.js
@@ -6,20 +6,21 @@ var blogText=document.getElementsByClassName("blogText")[0];
 var blogBody=document.getElementById("blogBody");
 var saveBtn=document.getElementById("postSaveBtn");
 editBtn.onclick=function(){
-    editBtn.style.display="none";
-    blogText.classList.add("editable");
+    editBtn.style.display="none"; //To hide the Edit Button
+    blogText.classList.add("editable"); //To make the content of the Blog Editable
     blogBody.setAttribute("contenteditable","true");
-    saveBtn.style.display="block";
+    saveBtn.style.display="block"; //To Display the Save Button
 }
 saveBtn.onclick=function(){
-    saveBtn.style.display="none";
-    blogText.classList.remove("editable");
+    saveBtn.style.display="none"; //To hide the Save Button
+    blogText.classList.remove("editable"); //To make the content of the Blog NON-Editable
     blogBody.removeAttribute("contenteditable");
-    editBtn.style.display="block";
+    editBtn.style.display="block"; //To Display Edit Button
 }
 var numLikes=0;
 var likeBtn=document.getElementById("likeBtn");
 var numLikesP=document.getElementById("numLikes");
+// To Display the number of likes and to change Button text from 'Like' to 'Liked!'
 likeBtn.onclick=function(){
     if(likeBtn.innerHTML=='Like <i class="fa fa-thumbs-up" aria-hidden="true"></i>')
         likeBtn.innerHTML='Liked! <i class="fa fa-thumbs-up" aria-hidden="true"></i>';
@@ -32,6 +33,7 @@ likeBtn.onclick=function(){
 var commentBtn=document.getElementById("commentBtn");
 var commentText=document.getElementById("commentTextarea");
 var commentDiv=document.getElementById("readComments");
+// To add comments to the page
 commentBtn.onclick=function(){   
     if(commentText.value!=""){
     var newPara=document.createElement("p");
@@ -41,7 +43,7 @@ commentBtn.onclick=function(){
         if((commentDiv.childNodes).length==0)
             commentDiv.appendChild(newPara);
         else
-            commentDiv.insertBefore(newPara,commentDiv.childNodes[0]);
+            commentDiv.insertBefore(newPara,commentDiv.childNodes[0]); //Make latest comment appear at the top
     }
     else{
         alert("Comment field can not be left empty.")
